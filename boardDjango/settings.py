f09e0ff7e28a324dbe89e76dc1f2cd07afd98ff8
Ubuntu <ubuntu@ip-172-31-18-212.ap-northeast-1.compute.internal>
2018-09-19 11:08:01 +0000

    20180919, writer : Lee, Working History : static, media setting files

--- boardDjango/settings.py
+++ boardDjango/settings.py
@@ -119,4 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'board', 'static')
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+STATIC_ROOT = os.path.join(BASE_DIR, '.static_root')
+# media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- boardDjango/urls.py
+++ boardDjango/urls.py
@@ -23,4 +23,8 @@ urlpatterns = [
     path('board/', include('board.urls')),
 ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
